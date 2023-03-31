250a5c7862b3a79f43e30bb4321dd3ffd37a1db1
Jay Iyamah <t-iyamah@live.co.uk>
2023-03-31 14:47:57 -0400

    'last test changes'

--- src/App.js
+++ src/App.js
@@ -5,31 +5,21 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import OddsDisplay from './pages/OddsDisplay/OddsDisplay';
 import HomePage from './pages/HomePage/HomePage';
 import FreePlay from './pages/FreePlay/FreePlay';
-<<<<<<< HEAD
 import NewsPage from './pages/NewsPage/NewsPage';
-=======
 import Submit from './pages/Submit/Submit';
->>>>>>> develop
 
 function App() {
   const [oddsType, setOddsType] = useState('american')
   return (
     <>
     <BrowserRouter>
-<<<<<<< HEAD
-    {/* <Header /> */}
-=======
->>>>>>> develop
       <Routes>
         <Route path='/' element={<HomePage oddsType={oddsType}/>} />
         <Route path='/oddsdisplay' element={<OddsDisplay oddsType={oddsType} setOddsType={setOddsType} />}/>
         <Route path='/profile' element = {<Profile/>} />
         <Route path='/freeplay' element={<FreePlay/>} />
-<<<<<<< HEAD
         <Route path='/news' element={<NewsPage/>} />
-=======
         <Route path='/submit' element={<Submit/>} />
->>>>>>> develop
       </Routes>
     </BrowserRouter>
     </>
