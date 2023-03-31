483fee5e3fee7adce19cb876bd4d097fdeae29e5
Sandun Kanangama <sandunkanangama@gmail.com>
2023-03-31 11:24:34 -0400

    update assets folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.scss';
+import { useState } from 'react'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import OddsDisplay from './pages/OddsDisplay/OddsDisplay';
 
 function App() {
+
+  const [oddsType, setOddsType] = useState('american')
+
   return (
     <>
     <BrowserRouter>
       <Routes>
-        <Route path='/oddsdisplay' element={<OddsDisplay />}/>
+        <Route path='/oddsdisplay' element={<OddsDisplay oddsType={oddsType} setOddsType={setOddsType} />}/>
       </Routes>
     </BrowserRouter>
     </>
