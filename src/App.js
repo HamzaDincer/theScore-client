381d542043e21e8175e505194de626d615ce7a65
Sandun Kanangama <sandunkanangama@gmail.com>
2023-03-30 21:31:04 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.scss';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Settings from './pages/Settings/Settings';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/settings' element={<Settings />}/>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
