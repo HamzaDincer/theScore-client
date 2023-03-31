c38bf6d36cc3bcd28e5a029510d5eb707ad6423d
Jay Iyamah <t-iyamah@live.co.uk>
2023-03-31 11:52:06 -0400

    added to feature freePlay

---
+++ src/component/freePlay/freePlay.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../../assets/5 for 500.svg";
+
+const freePlay = () => {
+  return (
+    <div className="freePlay-container">
+      <header className="freePlay-header">
+        <img src={logo} alt="5-for-500-logo" />
+      </header>
+    </div>
+  );
+};
+
+export default freePlay;
